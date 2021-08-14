7818028944eddf6a746a0ff5d663a3272265c275
EugeneZhdanov <jeka7753191@gmail.com>
2021-08-14 12:48:00 +0300

    adds finaly changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import Layout from './components/Layout/Layout';
+import Form from './components/Form/Form';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
+
+import useLocalStorage from './hooks/useLocalStorage';
+
+import { v4 as uuidv4 } from 'uuid';
+
+export default function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (text, number) => {
+    const contact = {
+      name: text,
+      number,
+      id: uuidv4(),
+    };
+
+    const isExistedName = contacts.find(
+      contact => contact.name.toLowerCase() === text.toLowerCase(),
+    );
+    if (isExistedName) {
+      alert(text + ' is already in your contacts');
+      return;
+    }
+
+    setContacts(prev => [contact, ...prev]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prev => {
+      return prev.filter(contact => contact.id !== contactId);
+    });
+  };
+
+  const handleFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+    return filteredContacts;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <h1>PhoneBook</h1>
+      <Form onSubmit={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilter} />
+      <ContactList
+        data={getFilteredContacts()}
+        onDeleteContact={deleteContact}
+      />
+    </Layout>
   );
 }
-
-export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import styles from './Form.module.css';
+
+export default function Form({ onSubmit }) {
+  const [contactName, setContactName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+    switch (name) {
+      case 'name':
+        setContactName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit(contactName, number);
+    reset();
+  };
+
+  const reset = () => {
+    setContactName('');
+    setNumber('');
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className={styles.label}>
+        Name
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          value={contactName}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <label className={styles.label}>
+        Phone Number
+        <input
+          className={styles.input}
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <button className={styles.button} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+}
